d406d711d71421781ea0cb41957539e5a2db266e
BaoHan260304 <nguyenmanhquan260304@gmail.com>
2026-09-11 18:57:57 +0700

    Lab 2 ex2

--- Lab2/dart_essentials.dart
+++ Lab2/dart_essentials.dart
@@ -8,6 +8,8 @@ void main() async {
   print("--- BẮT ĐẦU LAB 2 ---\n");
   exercise1();
   print("--------------------\n");
+  exercise2();
+  print("--------------------\n");
   print("\n--- KẾT THÚC LAB 2 ---");
 }
 
@@ -25,9 +27,43 @@ void exercise1() {
 
   // Sử dụng String interpolation (chèn biến trực tiếp vào chuỗi bằng dấu $)
   print("My name is $name, I am $age years old.");
-  print("My GPA is $gpa. Am I a student? $isStudent.");
+  print("My GPA is $gpa. \nAm I a student? $isStudent.");
 
   // Tính toán trực tiếp trong chuỗi bằng ${}
   print("Next year, I will be ${age + 1} years old.");
 }
 
+// ------------------------------------------
+// Exercise 2 – Collections & Operators
+// ------------------------------------------
+void exercise2() {
+  print("Exercise 2: Collections & Operators");
+
+  // 1. List (Danh sách có thứ tự, cho phép trùng lặp)
+  List<int> numbers = [10, 20, 30];
+  numbers.add(40); // Thêm 40
+  numbers.remove(10); // Xoá 10
+  print("List sau khi add và remove: $numbers");
+  print("Phần tử đầu tiên: ${numbers[0]}");
+
+  // 2. Set (Danh sách không có thứ tự, KHÔNG cho phép trùng lặp)
+  // Do để trùng lặp nên IDE cũng cảnh báo
+  Set<String> uniqueNames = {"Quan", "Binh", "Quan"};
+  print("Set (loại bỏ phần tử trùng): $uniqueNames");
+
+  // 3. Map (Lưu trữ theo cặp Khóa-Giá trị)
+  Map<String, String> capitals = {
+    "Vietnam": "Hanoi",
+    "Japan": "Tokyo"
+  };
+  capitals["France"] = "Paris"; // Thêm cặp mới
+  print("Thủ đô của Vietnam là: ${capitals["Vietnam"]}");
+
+  // 4. Toán tử (Operators)
+  int a = 5;
+  int b = 10;
+
+  // Toán tử 3 ngôi (? :)
+  String result = (a < b && b == 10) ? "Điều kiện ĐÚNG" : "Điều kiện SAI";
+  print("Toán tử 3 ngôi: $result");
+}
